daf637495ca949fe0aaac1b1feb3b70154c30f97
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2018-10-04 11:11:50 +0100

    Kata2 v3.1

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,29 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sarune Urvikyte
+ */
+
+public class Histogram {
+    
+    private final int[] data;
+
+    public int[] getData() {
+        return data;
+    }
+
+    public Histogram(int[] data) {
+        this.data = data;
+    }
+
+    public Map getHistogram(){
+        Map<Integer, Integer> histogram = new HashMap<>();
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }
+        return histogram;
+    }
+}
