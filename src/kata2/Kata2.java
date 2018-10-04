378ec17b486ee1912e5013f2e08d5dc7c5d84d58
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2018-10-04 10:53:03 +0100

    Kata2 v2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -16,16 +16,19 @@ public class Kata2 {
      */
     
     public static void main(String[] args) {
-        for (int i = 0; i < data.length; i++) {
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }
+        /*for (int i = 0; i < data.length; i++) {
                 if(histogram.containsKey(data[i])){
                     histogram.put(data[i], histogram.get(data[i])+1);
                 }else{
                     histogram.put(data[i], 1);
                 }
         }
-        
+        */
         histogram.entrySet().forEach((entry) -> {
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
-        }); 
+        });
     }  
 }
