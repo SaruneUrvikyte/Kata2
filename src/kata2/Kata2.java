517b068145d539d596fe0d29a0fd9c91a081e8fd
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2018-10-04 11:11:10 +0100

    Kata2 v3

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,33 +1,24 @@
 package kata2;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Sarune Urvikyte
  */
-public class Kata2 {
 
-    private static Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-    private static int[] data = {1, 2, 100, 45, 32, 7, 0, 100, 32, 1, 100, 32, 7, 100, 7, 9, 13, 100, 26, 86};
+public class Kata2 {
     
+    private final static int[] data = {1, 2, 100, 45, 32, 7, 0, 100, 32, 1, 100, 32, 7, 100, 7, 9, 13, 100, 26, 86};
+
     /**
      * @param args the command line arguments
      */
     
     public static void main(String[] args) {
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-        }
-        /*for (int i = 0; i < data.length; i++) {
-                if(histogram.containsKey(data[i])){
-                    histogram.put(data[i], histogram.get(data[i])+1);
-                }else{
-                    histogram.put(data[i], 1);
-                }
-        }
-        */
-        histogram.entrySet().forEach((entry) -> {
+        
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        histogr.entrySet().forEach((entry) -> {
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
         });
     }  
